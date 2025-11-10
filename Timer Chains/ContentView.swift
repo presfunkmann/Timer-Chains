dbaaedeb67fba8c54e221b299a140b33092fb04d
Pres <preston.o.mann@gmail.com>
2025-11-10 12:15:18 -0600

    Initial Commit

---
+++ Timer Chains/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timer Chains
+//
+//  Created by Preston Mann on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timer Chains/Timer_ChainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_ChainsApp.swift
+//  Timer Chains
+//
+//  Created by Preston Mann on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Timer_ChainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
